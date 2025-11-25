b7fa7f12a773f8986c3e86c8b98f3dc41a990aa6
Leonardo Marqueti de Lima <94808998+leomarqueti@users.noreply.github.com>
2025-11-25 16:07:47 +0000

    Adicionando primeiro estilos no projeto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
+import styles from "./Home.module.css"; // Importação do Módulo
+
 function Home() {
-  return <h1>OficinaSync Em construçao</h1>;
+  return <h1 className={styles.title}>OficinaSync Em construçao</h1>;
 }
 
 export default Home;
