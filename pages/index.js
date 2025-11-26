63beed92dd352717e884d8333dcda2286ae7655f
Leonardo Marqueti de Lima <94808998+leomarqueti@users.noreply.github.com>
2025-11-26 01:11:28 +0000

    Melhorando desing pagina de construção

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
-import styles from "./Home.module.css"; // Importação do Módulo
+import styles from "./Home.module.css";
+import logo from "./assets/images/Logo.png";
+import Image from "next/image";
 
 function Home() {
-  return <h1 className={styles.title}>OficinaSync Em construçao</h1>;
+  return (
+    <div className={styles.container}>
+      <Image src={logo} />
+      <h1 className={styles.title}>OficinaSync Em construçao</h1>;
+    </div>
+  );
 }
 
 export default Home;
