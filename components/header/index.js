2b4a6995243fa8eab846791c2d3250f99500f727
Leonardo Marqueti de Lima <94808998+leomarqueti@users.noreply.github.com>
2025-12-02 00:57:45 +0000

    Melhorando o header

--- components/header/index.js
+++ components/header/index.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./Header.module.css";
 import Botao from "../botao";
 
+//Aqui é o modulo responsavel por cuidar da HEADER do Site principal, Se quiser modificar algo é aqui
 function Header() {
   return (
     <header className={styles.headerContainer}>
@@ -14,11 +15,11 @@ function Header() {
           //Caminho da imagem
           src="/assets/images/Logo.png"
           //h é a altura , w para largura auto e cursor-pointer para a maozinhe o contain server para nao achatar a imagem
-          className="h-20 w-auto cursor-pointer object-contain"
+          className="h-10 w-auto cursor-pointer object-contain"
         />
       </div>
-      <div className={styles.centerGroup}>
-        <nav className={styles.nav}>
+      <div className="flex gap-80 text-base font-bold text-white">
+        <nav className="flex gap-2 md:gap-32">
           <a className={styles.a} href="#">
             Home
           </a>
