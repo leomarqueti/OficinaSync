53a7d920ab4dbf94f266aceca4148aae0c29593e
Leonardo Marqueti de Lima <94808998+leomarqueti@users.noreply.github.com>
2025-12-06 17:08:13 +0000

    melhorando o hero

--- components/botao/index.js
+++ components/botao/index.js
@@ -1,4 +1,4 @@
-// essa costante é a base dos estilos do botao, todos os botoes vao ter os configuraçoes dessa constante, mudou aqui, muda tudo 
+// essa costante é a base dos estilos do botao, todos os botoes vao ter os configuraçoes dessa constante, mudou aqui, muda tudo
 const baseStyles = "transition-all duration-300";
 
 //Variaçoes de estilos dos botoes, se quiser modificar algo de algum botao é aqui.
@@ -6,6 +6,8 @@ const variants = {
   primary: "bg-sync-green hover:opacity-50 text-black rounded-xl h-12 px-4", // Botão (Login)
   secondary:
     "bg-transparent border-2 border-sync-green text-sync-green hover:bg-sync-green hover:text-black rounded-xl h-12 px-4",
+  demostration:
+    "bg-sync-green hover:opacity-50 text-black rounded-xl w-44 h-12 px-4 font-bold",
 };
 
 // Aqui temos a funcão principal do botao, a base de tudo esta aqui ,qualquer logica referente a ação do botao esta aqui
